1afa89ecd0ad300e0b8bc6e5610914b252bf0c7c
Florian Tischler <florian.tischler@gmx.net>
2021-11-05 11:43:27 +0100

    initial commit

---
+++ apps/demo-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-rxjs-zone-scheduler-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo-app';
+}
