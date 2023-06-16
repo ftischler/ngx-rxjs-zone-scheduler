05eb7eb79bfbc467fc6870660d2f6856991895be
Florian Tischler <Florian.Tischler@gmx.net>
2023-06-16 19:58:48 +0200

    chore(repo): update to v16

--- libs/ngx-rxjs-zone-scheduler/src/lib/rx-ng-zone-scheduler.spec.ts
+++ libs/ngx-rxjs-zone-scheduler/src/lib/rx-ng-zone-scheduler.spec.ts
@@ -6,10 +6,8 @@ import { delay, skip } from 'rxjs/operators';
 import { RxNgZoneScheduler } from './rx-ng-zone-scheduler';
 
 class MockNgZone {
-  public run = jest.fn((fn) => this.ngZone.run(fn)).mockName('run');
-  public runOutsideAngular = jest
-    .fn((fn) => this.ngZone.runOutsideAngular(fn))
-    .mockName('runOutsideAngular');
+  public run = jest.fn((fn) => this.ngZone.run(fn));
+  public runOutsideAngular = jest.fn((fn) => this.ngZone.runOutsideAngular(fn));
 
   constructor(private ngZone: NgZone) {}
 }
@@ -67,82 +65,67 @@ describe('RxNgZoneScheduler', () => {
   });
 
   describe('observeOn operators', () => {
-    it(
-      'should call run on ngZone',
-      waitForAsync(() => {
-        expect.hasAssertions();
+    it('should call run on ngZone', waitForAsync(() => {
+      expect.hasAssertions();
 
-        const test$ = of('this is a test').pipe(service.observeOnNgZone());
+      const test$ = of('this is a test').pipe(service.observeOnNgZone());
 
-        test$.subscribe(() => {
-          expect(mockNgZone.run).toHaveBeenCalledTimes(2);
-          expect(mockNgZone.runOutsideAngular).not.toHaveBeenCalled();
-        });
-      })
-    );
+      test$.subscribe(() => {
+        expect(mockNgZone.run).toHaveBeenCalledTimes(2);
+        expect(mockNgZone.runOutsideAngular).not.toHaveBeenCalled();
+      });
+    }));
 
-    it(
-      'should not re-enter the Angular zone if the value has been emitted within the Angular zone',
-      waitForAsync(() => {
-        expect.hasAssertions();
+    it('should not re-enter the Angular zone if the value has been emitted within the Angular zone', waitForAsync(() => {
+      expect.hasAssertions();
 
-        const test$ = new Subject<string>();
+      const test$ = new Subject<string>();
 
-        test$.pipe(service.observeOnNgZone(), skip(1)).subscribe(() => {
-          // Previously, it would've been called 3 times, since we call it manually through `mockNgZone.run`
-          // and the `EnterZoneScheduler` would've called it 2 times.
-          expect(mockNgZone.run).toHaveBeenCalledTimes(2);
-        });
+      test$.pipe(service.observeOnNgZone(), skip(1)).subscribe(() => {
+        // Previously, it would've been called 3 times, since we call it manually through `mockNgZone.run`
+        // and the `EnterZoneScheduler` would've called it 2 times.
+        expect(mockNgZone.run).toHaveBeenCalledTimes(2);
+      });
 
-        mockNgZone.run(() => test$.next('within the Angular zone'));
-        mockNgZone.runOutsideAngular(() =>
-          test$.next('outside of the Angular zone')
-        );
-      })
-    );
+      mockNgZone.run(() => test$.next('within the Angular zone'));
+      mockNgZone.runOutsideAngular(() =>
+        test$.next('outside of the Angular zone')
+      );
+    }));
 
-    it(
-      'should call runOutsideAngular on ngZone',
-      waitForAsync(() => {
-        expect.hasAssertions();
+    it('should call runOutsideAngular on ngZone', waitForAsync(() => {
+      expect.hasAssertions();
 
-        const test$ = of('this is a test').pipe(service.observeOutOfNgZone());
+      const test$ = of('this is a test').pipe(service.observeOutOfNgZone());
 
-        test$.subscribe(() => {
-          expect(mockNgZone.runOutsideAngular).toHaveBeenCalledTimes(2);
-          expect(mockNgZone.run).not.toHaveBeenCalled();
-        });
-      })
-    );
+      test$.subscribe(() => {
+        expect(mockNgZone.runOutsideAngular).toHaveBeenCalledTimes(2);
+        expect(mockNgZone.run).not.toHaveBeenCalled();
+      });
+    }));
   });
 
   describe('subscribeOn operators', () => {
-    it(
-      'should call run on ngZone',
-      waitForAsync(() => {
-        expect.hasAssertions();
+    it('should call run on ngZone', waitForAsync(() => {
+      expect.hasAssertions();
 
-        const test$ = of('this is a test').pipe(service.subscribeOnNgZone());
+      const test$ = of('this is a test').pipe(service.subscribeOnNgZone());
 
-        test$.subscribe(() => {
-          expect(mockNgZone.run).toHaveBeenCalledTimes(1);
-          expect(mockNgZone.runOutsideAngular).not.toHaveBeenCalled();
-        });
-      })
-    );
+      test$.subscribe(() => {
+        expect(mockNgZone.run).toHaveBeenCalledTimes(1);
+        expect(mockNgZone.runOutsideAngular).not.toHaveBeenCalled();
+      });
+    }));
 
-    it(
-      'should call runOutsideAngular on ngZone',
-      waitForAsync(() => {
-        expect.hasAssertions();
+    it('should call runOutsideAngular on ngZone', waitForAsync(() => {
+      expect.hasAssertions();
 
-        const test$ = of('this is a test').pipe(service.subscribeOutOfNgZone());
+      const test$ = of('this is a test').pipe(service.subscribeOutOfNgZone());
 
-        test$.subscribe(() => {
-          expect(mockNgZone.runOutsideAngular).toHaveBeenCalledTimes(1);
-          expect(mockNgZone.run).not.toHaveBeenCalled();
-        });
-      })
-    );
+      test$.subscribe(() => {
+        expect(mockNgZone.runOutsideAngular).toHaveBeenCalledTimes(1);
+        expect(mockNgZone.run).not.toHaveBeenCalled();
+      });
+    }));
   });
 });
